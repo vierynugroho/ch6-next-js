b55a51337e37fa849394c2dd626d598f5ecb8741
Viery Nugroho <viery15102002@gmail.com>
2024-05-02 21:23:54 +0700

    initial commit

---
+++ libs/meals.js
@@ -0,0 +1,10 @@
+import SQLite from 'better-sqlite3';
+
+const database = SQLite('meals.db');
+
+export const getMeals = async () => {
+	await new Promise((resolve) => setTimeout(resolve, 3000));
+
+	throw new Error('Failed Loading Your Meals...');
+	return database.prepare('SELECT * FROM meals').all();
+};
